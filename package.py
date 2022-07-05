08b917fb456c204b21893e994eba215507ecb337
Runbutton <105370586+Runbutton@users.noreply.github.com>
2022-07-05 14:19:55 -0400

    Create package.py
    Is this necessary to port? Pulled from old bot repo: https://github.com/Bladelist/BBL-Bot/blob/main/package.json

---
+++ package.py
@@ -0,0 +1,18 @@
+bot_metadata = {
+            "name" : "Bladelist-bot",
+            "version" : "0.0.1",
+            "description" : "A bot made for devs to interact with the bladelist api.",
+            "main" : "", #not sure yet
+            "scripts" : "" #not sure yet
+                }
+
+author_metadata = {
+                  "author" : "LrnzDC3696",
+                  "license" : "MIT",
+                  "devDependencies" : "" #not sure yet.
+                  }
+
+
+dependencies = {
+                 #not sure yet
+               }
